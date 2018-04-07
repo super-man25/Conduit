047532fef7eab7165a14f033e32d3fc8a8995a1b
super-man25 <superman.star25@gmail.com>
2018-04-07 13:05:45 -0500

    SOS-79 add urlConstants (not being used yet)

---
+++ src/_constants/url.constants.js
@@ -0,0 +1,5 @@
+export const urlConstants = {
+    LOCAL_API_URL: 'http://localhost:9000',
+    QA_API_URL: 'http://qaapi.eventdynamic.com',
+    PRODUCTION_API_URL: 'http://api.eventdynamic.com'
+};
