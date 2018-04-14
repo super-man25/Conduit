6c3a55ac0d90254636783ccfcdd5e86f42728f4e
super-man25 <superman.star25@gmail.com>
2018-04-13 19:52:58 -0500

    SOS-183 Updates to some components to satisfy eslinter, without breaking the site styling...

--- src/_components/HelpBlockDiv.js
+++ src/_components/HelpBlockDiv.js
@@ -2,7 +2,15 @@ import styled from 'styled-components';
 import { cssConstants } from '../_constants';
 
 export const HelpBlockDiv = styled.div`
-  color: ${(props) => (props.type && props.type === 'alert-danger' ? cssConstants.SECONDARY_RED : props.type && props.type === 'alert-success' ? cssConstants.SECONDARY_GREEN : props.type && props.type === 'hint' ? cssConstants.SECONDARY_PURPLE : cssConstants.PRIMARY_LIGHT_BLACK)};
+  color: ${(props) => {
+    if (props.type && props.type === 'alert-danger') {
+      return cssConstants.SECONDARY_RED;
+    } else if (props.type && props.type === 'alert-success') {
+      return cssConstants.SECONDARY_GREEN;
+    } else if (props.type && props.type === 'hint') {
+      return cssConstants.SECONDARY_PURPLE;
+    } return cssConstants.PRIMARY_LIGHT_BLACK;
+  }};
   font-size: 0.9em;
   font-weight: 200;
   width: 100%;
@@ -11,5 +19,5 @@ export const HelpBlockDiv = styled.div`
   margin-top: 0;
   margin-bottom : 15px;
   padding: 0;
-  visibility: ${(props) => (props.show ? 'visible' : 'hidden')};
+  visibility: ${(props) => { return props.show ? 'visible' : 'hidden'; }};
 `;
--- src/_components/Input.js
+++ src/_components/Input.js
@@ -23,12 +23,25 @@ export const Input = styled.input`
   display: block;
   margin-top: 10px;
   margin-bottom : 5px;
-  background: ${(props) => (props.disabled ? cssConstants.PRIMARY_LIGHT_GRAY : cssConstants.PRIMARY_WHITE)};
-  background-image: ${(props) => (props.valid ? OK : props.inValid ? BAD : 'none')};
+  background: ${(props) => { return props.disabled ? cssConstants.PRIMARY_LIGHT_GRAY : cssConstants.PRIMARY_WHITE; }};
+  background-image: ${(props) => {
+    if (props.valid) {
+      return OK;
+    } else if (props.inValid) {
+      return BAD;
+    } return 'none';
+  }};
+;       
   background-repeat: no-repeat;
   background-position: right center;
   &:-webkit-autofill, -webkit-autofill:hover, -webkit-autofill:focus {
-    -webkit-animation-name: ${(props) => (props.valid ? autofillOK(true) : props.inValid ? autofillOK(false) : 'none')};
+    -webkit-animation-name: ${(props) => {
+    if (props.valid) {
+      return autofillOK(true);
+    } else if (props.inValid) {
+      return autofillOK(false);
+    } return 'none';
+  }};
     -webkit-animation-fill-mode: both;
   }
   font-size: 13.5px;
@@ -37,7 +50,13 @@ export const Input = styled.input`
   padding-left: 3%;
   border: 2px solid;
   border-radius: 3px;
-  border-color: ${(props) => (props.valid ? cssConstants.SECONDARY_GREEN : props.inValid ? cssConstants.SECONDARY_RED : cssConstants.PRIMARY_DARK_GRAY)};
+  border-color: ${(props) => {
+    if (props.valid) {
+      return cssConstants.SECONDARY_GREEN;
+    } else if (props.inValid) {
+      return cssConstants.SECONDARY_RED;
+    } return cssConstants.PRIMARY_DARK_GRAY;
+  }};
   padding-top: 0.9em;
   padding-bottom: 0.9em;
 `;
