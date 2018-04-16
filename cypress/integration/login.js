87466ee47d9c9f9aeb2890b0f0bbc4e01ae72f80
super-man25 <superman.star25@gmail.com>
2018-04-16 10:25:34 -0500

    updates test to use env variable

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,19 +1,19 @@
-describe('Login', function() {
-      // static data
-      beforeEach(function () {
-        // alias the users fixtures
-        cy.fixture('user.json').as('user');
-      })  
+describe('Login', () => {
 
-    it('can log into the application', function() {
-      // login page
-      cy.visit('/login');
-      cy.get('input[name="email"]').type(this.user.email);
-      cy.get('input[name="password"]').type(this.user.password);
-      cy.get('button').click();
-
-      // season dashboard -- assertion of url
-      cy.url().should('include', 'dashboard')
-    });
+  // static data
+  beforeEach(() => {
+    // alias the users fixtures
+    cy.fixture('user').as('user');
   });
-  
+
+  it('can log into the application', () => {
+    // login page
+    cy.visit('/login');
+    cy.get('input[name="email"]').type(this.user.email);
+    cy.get('input[name="password"]').type(Cypress.env('ED_USER_PASS'));
+    cy.get('button').click();
+
+    // season dashboard -- assertion of url
+    cy.url().should('include', 'dashboard');
+  });
+});
