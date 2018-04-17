9451da71292298c11ed27e6c0b14d88d00c9ab79
super-man25 <superman.star25@gmail.com>
2018-04-16 22:14:19 -0500

    SOS-108 Updates to frontend Team Settings panel of SettingsPage component to enable updating the price update interval.  Really, this time ;-)

--- src/_components/LeftNav.js
+++ src/_components/LeftNav.js
@@ -6,9 +6,13 @@ export const LeftNav = styled.div`
   width: calc(19% - 41px);
   clear: left;
   float: left;
-  height: calc(100% - 40px);
+  top: 0;
+  bottom: 0;
+  min-height: calc(100% - 40px);
+  height: auto;
   margin: 0;
   padding: 20px;
+  margin-bottom: -5000px;
   background: ${cssConstants.PRIMARY_WHITE};
   border-right: 1px solid ${cssConstants.PRIMARY_LIGHT_GRAY};
 `;
