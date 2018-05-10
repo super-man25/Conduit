96c21d9e4eb50f0a26efab0ad8dfe3dbadd6a574
super-man25 <superman.star25@gmail.com>
2018-05-10 13:23:42 -0500

    added snapshot test

--- src/_components/__tests__/TeamOverview.test.js
+++ src/_components/__tests__/TeamOverview.test.js
@@ -4,7 +4,8 @@ import renderer from 'react-test-renderer';
 
 // this will evolve to test winLoss and gamesRemaining props, when component is fleshed out...
 
-it('renders correctly with no props', () => {
-  const tree = renderer.create(<TeamOverview />).toJSON();
+it('renders correctly with props', () => {
+  const stats = Object({ wins: 10, losses: 1, gamesRemsining: 1 });
+  const tree = renderer.create(<TeamOverview onToggleSidebar="" stats={stats}/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
