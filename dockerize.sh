95599cef9e60c3cda928c7c89eeed33db1c3124d
super-man25 <superman.star25@gmail.com>
2018-08-11 10:43:50 -0500

    Consistently build, tag, and push docker images (#198)
    * add sbt-docker plugin/commands
    
    * enable docker builds for ticket fulfillment pipeline
    
    * update ticketSync docker build.sbt settings
    
    * update build.sbt
    
    * add docker settings for inventory sync project
    
    * Add/update dockerfiles
    
    * add yml to editorconfig
    
    * remove sbt-docker
    
    * update ticket-sync dockerfile
    
    * remove sbt-docker plugin
    
    * add dockerize scripts
    
    * update dockerize scripts to include git hash and latest tags
    
    * update dockerize scripts
    
    * add inventory-sync job config
    
    * update dockerfile
    
    * update anyconnect server env var in marathon config

---
+++ dockerize.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# Project-specific variables
+BUILD_DIR="."
+DOCKER_IMAGE="ed-web"
+BUILD_COMMAND="yarn build-qa"
+
+# Shared variables
+SHOULD_PUSH=$1
+DOCKER_ORG="dialexa"
+DOCKER_TAG_HASH="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)"
+DOCKER_TAG_LATEST="latest"
+
+# Build project assets
+printf "\n> Running: $BUILD_COMMAND\n\n"
+$BUILD_COMMAND
+
+# Build docker image
+printf "\n> Building Docker Images: $DOCKER_ORG/$DOCKER_IMAGE\n\n"
+docker build --no-cache --rm --compress --tag $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH $BUILD_DIR
+docker tag $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_LATEST
+
+# Push docker images
+if [[ "$SHOULD_PUSH" != "" && "$SHOULD_PUSH" == true ]]; then
+	printf "\n> Pushing Docker Images to Docker Hub: $DOCKER_ORG/$DOCKER_IMAGE\n\n"
+	docker push $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH
+	docker push $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_LATEST
+else
+	printf "\n> NOT pushing Docker Images to Docker Hub\n\n"
+fi
