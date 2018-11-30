5af6e00f74ff2358aa75b241eebbba4e8eef970a
super-man25 <superman.star25@gmail.com>
2018-11-30 13:17:34 -0600

    Some basics working

---
+++ src/_scenes/Settings/Demo/constants.js
@@ -0,0 +1,82 @@
+// Dropdown options
+
+export const WEATHER_CONDITIONS = [
+  {
+    title: 'Clear',
+    key: 'clear'
+  },
+  {
+    title: 'Cloudy',
+    key: 'clouds'
+  },
+  {
+    title: 'Heavy Clouds',
+    key: 'clouds_heavy'
+  },
+  {
+    title: 'Light Rain',
+    key: 'rain_light'
+  },
+  {
+    title: 'Rain',
+    key: 'rain'
+  }
+];
+
+export const MONTHS = [
+  {
+    title: 'April',
+    key: 4
+  },
+  {
+    title: 'May',
+    key: 5
+  },
+  {
+    title: 'June',
+    key: 6
+  },
+  {
+    title: 'July',
+    key: 7
+  },
+  {
+    title: 'August',
+    key: 8
+  },
+  {
+    title: 'September',
+    key: 9
+  }
+];
+
+export const DAYS_OF_WEEK = [
+  {
+    title: 'Monday',
+    key: 0
+  },
+  {
+    title: 'Tuesday',
+    key: 1
+  },
+  {
+    title: 'Wednesday',
+    key: 2
+  },
+  {
+    title: 'Thursday',
+    key: 3
+  },
+  {
+    title: 'Friday',
+    key: 4
+  },
+  {
+    title: 'Saturday',
+    key: 5
+  },
+  {
+    title: 'Sunday',
+    key: 6
+  }
+];
--- src/_scenes/Settings/Demo/index.js
+++ src/_scenes/Settings/Demo/index.js
@@ -3,10 +3,12 @@ import React from 'react';
 import {
   Flex,
   H3,
+  H4,
+  Label,
   PageWrapper,
   PrimaryContent,
   S1,
-  Spacing
+  Dropdown
 } from '_components';
 import { connect } from 'react-redux';
 import {
@@ -14,14 +16,15 @@ import {
   type State as DemoState
 } from '_state/demo';
 import { DemoPriceContext, DemoPriceExample } from '_models';
+import { MONTHS, DAYS_OF_WEEK, WEATHER_CONDITIONS } from './constants';
 
 type State = {
-  demoState: DemoState,
   context: DemoPriceContext,
   examples: Array<DemoPriceExample>
 };
 
 type Props = {
+  demoState: DemoState,
   fetch: (context: DemoPriceContext, examples: Array<DemoPriceExample>) => void
 };
 
@@ -30,6 +33,15 @@ const DemoWrapper = Flex.extend`
   justify-content: left;
   min-width: 100%;
 `;
+
+const DataWrapper = Flex.extend`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const parseOption = (option) => option.title;
+const getSelectedOption = (options, key) => options.find((o) => o.key === key);
 
 class DemoRoute extends React.Component<Props, State> {
   state = {
@@ -69,11 +81,40 @@ class DemoRoute extends React.Component<Props, State> {
     ]
   };
 
-  handleContextChange() {
-    this.props.fetch(this.state.context, this.state.examples);
+  componentDidMount() {
+    this.fetchPrices();
   }
 
+  onCheckboxChange = (feature) => (event) => {
+    let context = { ...this.state.context };
+    context[feature] = event.target.checked;
+    this.setState({ context }, this.fetchPrices);
+  };
+
+  onDropdownChange = (feature) => (option) => {
+    let context = { ...this.state.context };
+    context[feature] = option.key;
+    this.setState({ context }, this.fetchPrices);
+  };
+
+  fetchPrices = () => {
+    this.props.fetch(this.state.context, this.state.examples);
+  };
+
   render() {
+    const {
+      event_month,
+      event_day_of_week,
+      home_opener,
+      games_ahead,
+      win_loss_ratio,
+      weather_condition,
+      temp
+    } = this.state.context;
+
+    const prices = this.props.demoState.prices;
+    const examples = this.state.examples;
+
     return (
       <PageWrapper>
         <PrimaryContent padding="2rem">
@@ -82,7 +123,67 @@ class DemoRoute extends React.Component<Props, State> {
             <S1 weight="300">
               <i>Pricing model demo.</i>
             </S1>
-            <button onClick={this.handleContextChange()}>Press me.</button>
+            <DataWrapper>
+              <div>
+                <H4>Event Date</H4>
+
+                <Label htmlFor="monthDrop">Month</Label>
+                <Dropdown
+                  id="monthDrop"
+                  selected={getSelectedOption(MONTHS, event_month)}
+                  onChange={this.onDropdownChange('event_month')}
+                  options={MONTHS}
+                  parseOption={parseOption}
+                />
+
+                <Label htmlFor="dayDrop">Day of week</Label>
+                <Dropdown
+                  id="dayDrop"
+                  selected={getSelectedOption(DAYS_OF_WEEK, event_day_of_week)}
+                  onChange={this.onDropdownChange('event_day_of_week')}
+                  options={DAYS_OF_WEEK}
+                  parseOption={parseOption}
+                />
+
+                <Label htmlFor="homeOpener">Home Opener</Label>
+                <input
+                  type="checkbox"
+                  name="homeOpener"
+                  value={this.state.context.home_opener}
+                  onChange={this.onCheckboxChange('home_opener')}
+                />
+
+                <H4>Performance</H4>
+
+                <Label>Games Ahead</Label>
+
+                <Label>Win Loss Ratio</Label>
+
+                <H4>Weather</H4>
+
+                <Label htmlFor="condDrop">Weather Condition</Label>
+                <Dropdown
+                  id="condDrop"
+                  selected={getSelectedOption(
+                    WEATHER_CONDITIONS,
+                    weather_condition
+                  )}
+                  onChange={this.onDropdownChange('weather_condition')}
+                  options={WEATHER_CONDITIONS}
+                  parseOption={parseOption}
+                />
+              </div>
+              <div>
+                <ul>
+                  {examples.map((example, idx) => (
+                    <li key={idx}>
+                      {example.degrees} - {example.distance} -{' '}
+                      {example.row_number} {prices ? '- $' + prices[idx] : ''}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </DataWrapper>
           </DemoWrapper>
         </PrimaryContent>
       </PageWrapper>
@@ -90,7 +191,9 @@ class DemoRoute extends React.Component<Props, State> {
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  demoState: state.demoPrice
+});
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   fetch: (context, examples) =>
