9c7e52cea0cb0485582ff3239e8e04474c0c4550
super-man25 <superman.star25@gmail.com>
2018-12-03 16:33:38 -0600

    Add qa tag build to ed web

--- dockerize.sh
+++ dockerize.sh
@@ -12,6 +12,7 @@ SHOULD_PUSH=$1
 DOCKER_ORG="dialexa"
 DOCKER_TAG_HASH="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)"
 DOCKER_TAG_LATEST="latest"
+DOCKER_TAG_QA="qa"
 
 # Build project assets
 printf "\n> Running: $BUILD_COMMAND\n\n"
@@ -21,12 +22,14 @@ $BUILD_COMMAND
 printf "\n> Building Docker Images: $DOCKER_ORG/$DOCKER_IMAGE\n\n"
 docker build --no-cache --rm --compress --tag $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH $BUILD_DIR
 docker tag $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_LATEST
+docker tag $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_QA
 
 # Push docker images
 if [[ "$SHOULD_PUSH" != "" && "$SHOULD_PUSH" == true ]]; then
 	printf "\n> Pushing Docker Images to Docker Hub: $DOCKER_ORG/$DOCKER_IMAGE\n\n"
 	docker push $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH
 	docker push $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_LATEST
+	docker push $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_QA
 else
 	printf "\n> NOT pushing Docker Images to Docker Hub\n\n"
 fi
