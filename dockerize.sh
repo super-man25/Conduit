b7c4d5d5d01918cc617d63730d0d47560579bd61
super-man25 <superman.star25@gmail.com>
2019-01-23 16:24:43 -0600

    removing latest tags from docker builds

--- dockerize.sh
+++ dockerize.sh
@@ -20,7 +20,6 @@ BUILD_COMMAND="yarn build-$DOCKER_TAG_ENV"
 SHOULD_PUSH=$1
 DOCKER_ORG="dialexa"
 DOCKER_TAG_HASH="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)"
-DOCKER_TAG_LATEST="latest"
 
 # Build project assets
 printf "\n> Running: $BUILD_COMMAND\n\n"
@@ -29,14 +28,12 @@ $BUILD_COMMAND
 # Build docker image
 printf "\n> Building Docker Images: $DOCKER_ORG/$DOCKER_IMAGE\n\n"
 docker build --no-cache --rm --compress --tag $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH $BUILD_DIR
-docker tag $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_LATEST
 docker tag $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_ENV
 
 # Push docker images
 if [[ "$SHOULD_PUSH" != "" && "$SHOULD_PUSH" == true ]]; then
 	printf "\n> Pushing Docker Images to Docker Hub: $DOCKER_ORG/$DOCKER_IMAGE\n\n"
 	docker push $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_HASH
-	docker push $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_LATEST
 	docker push $DOCKER_ORG/$DOCKER_IMAGE:$DOCKER_TAG_ENV
 else
 	printf "\n> NOT pushing Docker Images to Docker Hub\n\n"
