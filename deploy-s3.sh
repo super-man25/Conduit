5ab6a373f8712c44bcb29bb03aeaf1cc99db32cf
super-man25 <superman.star25@gmail.com>
2019-08-30 11:00:51 -0500

    FIX: TDC proxy whitelist host filter for load balancer (#777)

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 BUCKET=$1
+CDN_DISTRIBUTION_ID=$2
 
 printf "\n> Syncing build with s3 bucket\n\n"
 aws s3 sync build/ s3://$BUCKET
+
+printf "\n> Invalidate Cloudfront Cache\n\n"
+aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"
