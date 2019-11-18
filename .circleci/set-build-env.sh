efe9373a8b557a9a7c50e15f71f7fd3a76f49737
super-man25 <superman.star25@gmail.com>
2019-11-18 12:32:44 -0600

    add circleci

---
+++ .circleci/set-build-env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+printf "> On $CIRCLE_BRANCH branch \n"
+
+if [[ "$CIRCLE_BRANCH" == "master" ]]; then
+  BUILD_ENV="prod"
+elif [[ "$CIRCLE_BRANCH" == "develop" ]]; then
+  BUILD_ENV="qa"
+fi
+
+printf "> Setting BUILD_ENV=${BUILD_ENV} \n"
+
+echo "export BUILD_ENV=${BUILD_ENV}" >> $BASH_ENV
