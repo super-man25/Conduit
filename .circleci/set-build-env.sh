5d86eaf442d701ec92ee3026b92f913c3ebf85c3
super-man25 <superman.star25@gmail.com>
2019-12-03 15:07:04 -0600

    Prod Release 12/3 (#13)

--- .circleci/set-build-env.sh
+++ .circleci/set-build-env.sh
@@ -8,6 +8,8 @@ if [[ "$CIRCLE_BRANCH" == "master" ]]; then
   BUILD_ENV="prod"
 elif [[ "$CIRCLE_BRANCH" == "develop" ]]; then
   BUILD_ENV="qa"
+elif [[ "$CIRCLE_BRANCH" == "demo" ]]; then
+  BUILD_ENV="demo"
 fi
 
 printf "> Setting BUILD_ENV=${BUILD_ENV} \n"
