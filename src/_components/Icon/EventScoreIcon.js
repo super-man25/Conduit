f17b099677122894417f16ca51a588c8e19a2710
super-man25 <superman.star25@gmail.com>
2019-12-16 16:30:51 -0600

    ED-1045: Event Score Icon (#23)

---
+++ src/_components/Icon/EventScoreIcon.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { cssConstants } from '_constants';
+
+const Background = styled.div`
+  background-color: ${cssConstants.PRIMARY_BLUE};
+  padding: 4px 12px; /* numbers chosen visually to make a ticket shape */
+  border-radius: 3px;
+  position: relative;
+  display: inline-block;
+  margin-bottom: 2px;
+
+  &:before,
+  &:after {
+    content: '';
+    width: 8px;
+    height: 8px;
+    background-color: white;
+    border-radius: 5px;
+    position: absolute;
+    top: calc(50% - 4px);
+  }
+
+  &:before {
+    right: calc(100% - 4px);
+  }
+
+  &:after {
+    left: calc(100% - 4px);
+  }
+`;
+
+const Text = styled.div`
+  color: white;
+`;
+
+export const EventScoreIcon = ({ eventScore }) => (
+  <Background>
+    <Text>{eventScore}</Text>
+  </Background>
+);
