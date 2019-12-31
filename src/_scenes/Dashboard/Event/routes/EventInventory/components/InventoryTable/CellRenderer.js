91f0b2f092e03e33db37a8bf5d6fc9de0b380066
super-man25 <superman.star25@gmail.com>
2019-12-30 18:59:02 -0600

    update default cell renderer

--- src/_scenes/Dashboard/Event/routes/EventInventory/components/InventoryTable/CellRenderer.js
+++ src/_scenes/Dashboard/Event/routes/EventInventory/components/InventoryTable/CellRenderer.js
@@ -1,21 +1,86 @@
 // @flow
 import * as React from 'react';
-import { isDefined } from '_helpers';
-import { Flex, Text } from '_components';
+import { connect } from 'react-redux';
+
+import { Flex, NumberInputField, Text } from '_components';
+import { actions } from '_state/eventInventory';
+import { ManualPricingInput } from './styled';
 
 type Props = {
-  cellData: any
+  cellData: any,
+  isEditing: boolean,
+  rulePropertyValue: string,
+  columnData: any,
+  updateEditedRowProperty: (any) => void,
+  isEditable: boolean
 };
 
-export const defaultCellRenderer = (props: Props) => {
-  const { cellData } = props;
-  const value = isDefined(cellData) ? String(cellData) : '';
-
-  return (
-    <Flex align="center" marginLeft="1.25rem">
-      <Text ellipsis title={value}>
-        {value}
-      </Text>
-    </Flex>
-  );
+type State = {
+  rulePropertyValue: string
 };
+
+class DefaultCellPresenter extends React.Component<Props, State> {
+  state = { rulePropertyValue: '' };
+
+  onRulePropertyChange = (e: SyntheticEvent<HTMLInputElement>) => {
+    const { value } = (e.currentTarget: HTMLInputElement);
+    const { updateEditedRowProperty } = this.props;
+
+    // convert empty string values to null
+    const newProperty = value === '' ? null : value;
+
+    updateEditedRowProperty(newProperty);
+  };
+
+  render() {
+    const {
+      isEditing,
+      cellData,
+      columnData: { isEditable }
+    } = this.props;
+
+    if (isEditing && isEditable) {
+      return (
+        <Flex align="center">
+          <NumberInputField
+            component={ManualPricingInput}
+            value={cellData}
+            onChange={this.onRulePropertyChange}
+            placeholder={'-'}
+          />
+        </Flex>
+      );
+    }
+    return (
+      <Flex align="center">
+        <Text>{cellData}</Text>
+      </Flex>
+    );
+  }
+}
+
+const mapStateToProps = (
+  { eventInventory: { editedRowId, loading } },
+  { rowData }
+) => ({
+  isEditing: editedRowId === rowData.id,
+  isLoading: loading
+});
+
+const mapDispatchToProps = (dispatch, { rowData, dataKey }) => ({
+  updateEditedRowProperty: (value) =>
+    dispatch(
+      actions.updateEditedRowProperty({
+        id: rowData.id,
+        propertyName: dataKey,
+        propertyValue: value
+      })
+    )
+});
+
+const DefaultCell = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DefaultCellPresenter);
+
+export const defaultCellRenderer = (props: Props) => <DefaultCell {...props} />;
