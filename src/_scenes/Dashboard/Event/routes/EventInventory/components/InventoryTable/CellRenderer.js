0a3d513bf2cd6b1cef1d7563b869f0bcff23884e
super-man25 <superman.star25@gmail.com>
2020-01-07 15:00:25 -0600

    string -> float

--- src/_scenes/Dashboard/Event/routes/EventInventory/components/InventoryTable/CellRenderer.js
+++ src/_scenes/Dashboard/Event/routes/EventInventory/components/InventoryTable/CellRenderer.js
@@ -75,7 +75,7 @@ const mapDispatchToProps = (dispatch, { rowData, dataKey }) => ({
       actions.updateEditedRowProperty({
         id: rowData.id,
         propertyName: dataKey,
-        propertyValue: value
+        propertyValue: parseFloat(value)
       })
     )
 });
